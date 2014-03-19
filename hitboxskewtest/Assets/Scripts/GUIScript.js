3a451b7de96b01e29f5ac83744c9770ce1c4cf9b
Jonathan Ballands <jballands@gmail.com>
2014-03-19 16:17:13 -0400

    Outline for shadow testing

---
+++ hitboxskewtest/Assets/Scripts/GUIScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var shadowSlider : float;
+
+function Start () {
+	// Nothing to do...
+}
+
+function Update () {
+	// Nothing to do...
+}
+
+function OnGUI () {
+	// Get the value of the shadow slider
+	shadowSlider = GUI.HorizontalSlider(Rect(25, 25, 200, 30), shadowSlider, 0, 1);
+}
---
+++ hitboxskewtest/Assets/Scripts/ShadowScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+
+// The GUI
+private var gui : GameObject;
+private var sliderValue : float;
+
+function Start () {
+
+	// Find the GUI
+	gui = GameObject.Find("GUI");
+	
+	// Store the value somewhere
+	sliderValue = gui.GetComponent(GUIScript).shadowSlider;
+}
+
+function Update () {
+	
+	// TODO: Do something...
+	
+}
