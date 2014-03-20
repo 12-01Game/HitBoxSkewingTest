23fb272ae8315aaf00eafa5b8f6cffdf6c760bff
Jonathan Ballands <jballands@gmail.com>
2014-03-20 16:46:01 -0400

    THERES A MESH!!! THERES A MESH!!!!!!!!!!!!dagd;iosgjdas;ggarhgrwh4rhq,3q4

--- hitboxskewtest/Assets/Scripts/ShadowScript.js
+++ hitboxskewtest/Assets/Scripts/ShadowScript.js
@@ -1,19 +1,36 @@
-﻿#pragma strict
+﻿/*
+ *	ShadowScript.js
+ *
+ *	Version 0.0
+ *	(C)2014 Jonathan Ballands, All Rights Reserved.
+ *
+ *	Written for the 12:01 video game for CS 4644
+ */
 
+#pragma strict
 
-// The GUI
+/*
+ *	Fields
+ */
+ 
+var objToWallDistance : float;		// Store how far away the GameObject's edge is from the wall
+
+// GUI
 private var gui : GameObject;
 private var sliderValue : float;
 
-private var height : float;
-private var width : float;
-private var originX : float;
-private var originY : float;
-private var originZ : float;
+// Object properties
+private var objWidth : float;		// Stores the GameObject's width
+private var objHeight : float;		// Stores the GameObject's height
 
-private var mf : MeshFilter;
-private var vertices : Vector3[];
-private var mesh : Mesh;
+private var objOriginX : float;		// Stores where the GameObject is in space
+private var objOriginY : float;		// Stores where the GameObject is in space
+private var objOriginZ : float;		// Stores where the GameObject is in space
+
+// Shadow properties
+private var shadowMesh : Mesh;
+
+// TODO: Have some boolean variables (eg, isMiddle)
 
 /*
  *	Start()
@@ -28,26 +45,12 @@ function Start () {
 	// Store the value somewhere
 	sliderValue = gui.GetComponent(GUIScript).shadowSlider;
 	
-	// Get the mesh filter component and set it
-	var mf: MeshFilter = GetComponent(MeshFilter);
-	mesh = new Mesh();
-	mf.mesh = mesh;
+	// Initialize GameObject properties
+	objWidth = collider.bounds.size.z;
+	objHeight = collider.bounds.size.y;
 	
-	// Get the collider
-	var collider : BoxCollider = GetComponent(BoxCollider);
-	
-	// Initialize the variables
-	
-	// TODO: Setting the origin x, y, and z should be done in the Update() method
-	// to allow for objects that can be moved!!
-	vertices = new Vector3[4];
-	width = collider.bounds.size.z;
-	height = collider.bounds.size.y;
-	originX = collider.transform.position.x;
-	originY = collider.transform.position.y;
-	originZ = collider.transform.position.z;
-	
-	renderer.material.SetTexture();
+	// Go
+	ActivateShadow();
 }
 
 /*
@@ -57,28 +60,44 @@ function Start () {
  */
 function Update () {
 
-	var startX = originX - (originX / 2) - 10;
-	var startY = originY - (originY / 2);
-	var startZ = originZ - (originZ / 2);
+	// TODO: Make the shadow move
 	
-	// Set vertices
-	vertices[0] = new Vector3(startX, startY, startZ);
-	vertices[1] = new Vector3(startX, startY, startZ + width);
-	vertices[2] = new Vector3(startX, startY + height, startZ + width);
-	vertices[3] = new Vector3(startX, startY + height, startZ);
+}
+
+/*
+ *	ActivateShadow()
+ *
+ *	Kicks off the shadow creation process by generating a Mesh
+ *	for the shadow to reside on.
+ */
+function ActivateShadow() {
+
+	// Null check
+	if (shadowMesh != null) {
+		Debug.LogError("The shadow has already be activated for this GameObject");
+		return;
+	}
 	
-	mesh.vertices = vertices;
+	// Make a new shadow mesh
+	shadowMesh = new Mesh();
+	shadowMesh.name = "Shadow_Mesh_" + gameObject.name;
+
+	// Define vertices
+	shadowMesh.vertices = [Vector3(objOriginX + objToWallDistance, objOriginY, objOriginZ),
+						   Vector3(objOriginX + objToWallDistance, objOriginY, objOriginZ + objWidth),
+						   Vector3(objOriginX + objToWallDistance, objOriginY + objHeight, objOriginZ +objWidth),
+						   Vector3(objOriginX + objToWallDistance, objOriginY + objHeight, objOriginZ)];
+						   
+	// Define triangles
+	shadowMesh.triangles = [0, 1, 2, 0, 2, 3];
 	
-	// Code from website
-	var tri: int[] = new int[6];
-
-	tri[0] = 0;
-	tri[1] = 2;
-	tri[2] = 1;
-
-	tri[3] = 2;
-	tri[4] = 3;
-	tri[5] = 1;
-
-	mesh.triangles = tri;
+	// Define UVs
+	shadowMesh.uv = [Vector2 (0, 0), Vector2 (0, 1), Vector2(1, 1), Vector2 (1, 0)];
+	
+	// Define normals
+	shadowMesh.RecalculateNormals();
+	
+	// Create the shadow plane
+	var shadowObj : GameObject = new GameObject("Shadow_Object_" + gameObject.name, MeshRenderer, MeshFilter, MeshCollider);
+	shadowObj.GetComponent(MeshFilter).mesh = shadowMesh;
 }
